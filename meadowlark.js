c4530bca796b8d6e35d17e45e62c8fb691a09c6a
lxcao <caolingxin@qq.com>
2015-10-17 13:56:30 +0800

    Moved 'fortune cookie' forunctionality into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
@@ -11,7 +12,7 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
-
+/*
 var fortuneCookies = [
 	"Conquer your fears or they will conquer you.",
 	"Rivers need springs.",
@@ -19,14 +20,12 @@ var fortuneCookies = [
 	"You will have a pleasant surprise.",
 	"Whenever possible, keep it simple.",
 ];
-
+*/
 app.get('/', function(req, res) {
 	res.render('home');
 });
 app.get('/about', function(req,res){
-	var randomFortune = 
-		fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
-	res.render('about', { fortune: randomFortune });
+	res.render('about', { fortune: fortune.getFortune() });
 });
 
 // 404 catch-all handler (middleware)
